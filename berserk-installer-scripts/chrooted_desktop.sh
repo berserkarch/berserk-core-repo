573fc1e7a616ede7d5ac9ecb8f6fd968a728105d
Gaurav Raj <techw803@gmail.com>
2025-09-29 16:07:15 +0530

    removed predetermined files for network manager

--- berserk-installer-scripts/chrooted_desktop.sh
+++ berserk-installer-scripts/chrooted_desktop.sh
@@ -14,7 +14,7 @@ gnome_pkgs=('baobab' 'decibels' 'epiphany' 'evince' 'gdm' 'gnome-backgrounds' 'g
 
 xfce_pkgs=('exo' 'garcon' 'xfce4-appfinder' 'xfce4-panel' 'xfce4-power-manager' 'xfce4-session' 'xfce4-settings' 'xfce4-terminal' 'xfconf' 'xfdesktop' 'xfwm4' 'xfwm4-themes' 'mousepad' 'parole' 'ristretto' 'xfburn' 'xfce4-artwork' 'xfce4-battery-plugin' 'xfce4-clipman-plugin' 'xfce4-cpufreq-plugin' 'xfce4-cpugraph-plugin' 'xfce4-dict' 'xfce4-diskperf-plugin' 'xfce4-eyes-plugin' 'xfce4-fsguard-plugin' 'xfce4-genmon-plugin' 'xfce4-mailwatch-plugin' 'xfce4-mount-plugin' 'xfce4-mpc-plugin' 'xfce4-netload-plugin' 'xfce4-notes-plugin' 'xfce4-notifyd' 'xfce4-places-plugin' 'xfce4-pulseaudio-plugin' 'xfce4-screensaver' 'xfce4-screenshooter' 'xfce4-sensors-plugin' 'xfce4-smartbookmark-plugin' 'xfce4-systemload-plugin' 'xfce4-taskmanager' 'xfce4-time-out-plugin' 'xfce4-timer-plugin' 'xfce4-verve-plugin' 'xfce4-wavelan-plugin' 'xfce4-weather-plugin' 'xfce4-whiskermenu-plugin' 'xfce4-xkb-plugin' 'gnome-contacts' 'gnome-maps' 'gnome-music' 'decibels' 'simple-scan' 'epiphany' 'berserk-rofi' 'berserk-polybar' 'berserk-config-xfce')
 
-openbox_pkgs=('berserk-config-openbox')
+openbox_pkgs=('obmenu-generator' 'berserk-config-openbox' 'openbox' 'obconf-qt' 'xcompmgr' 'nitrogen' 'dunst')
 
 ## Install Packages ----------
 _install_packages() {
@@ -95,6 +95,7 @@ install_xfce() {
   echo "[*] Installing XFCE DE..."
   remove_openbox
   _install_packages xfce_pkgs
+  cp -r /usr/share/berserkarch/home/ "$HOME_DIR/"
 }
 
 ## Install XFCE ----------
