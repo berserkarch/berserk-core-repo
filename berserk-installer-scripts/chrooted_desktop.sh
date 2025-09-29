f4d3378fc4050dd220e61e4ff7320fe9a6c7c35f
Gaurav Raj <techw803@gmail.com>
2025-09-29 19:41:38 +0530

    added xfce configs

--- berserk-installer-scripts/chrooted_desktop.sh
+++ berserk-installer-scripts/chrooted_desktop.sh
@@ -97,7 +97,7 @@ install_xfce() {
   _install_packages xfce_pkgs
   rm -rf "$HOME_DIR/.config/xfce4"
   cp -r /usr/share/berserkarch/home/ "$HOME_DIR/"
-  sed -i 's|Session=.*|Session=/usr/share/xsessions/xfce.desktop|' /var/share/xsessions/xfce.desktop
+  sed -i 's|Session=.*|Session=/usr/share/xsessions/xfce.desktop|' /var/lib/sddm/state.conf
 }
 
 ## Install XFCE ----------
